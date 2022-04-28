f1d132933f37bf4f6a68e3d9efbb506dd2fc5f4b
GayeongChoi <wwuu12366@gmail.com>
2022-04-28 11:41:07 +0900

    add

---
+++ webs_class/game/musicPlayer/api/music.js
@@ -0,0 +1,122 @@
+let allMusic = [
+    {
+        name : "1.Échappé",
+        artist : "Jone Mickal",
+        img : "music-1",
+        audio : "music-1"
+    },
+    {
+        name : "2.illogical",
+        artist : "Jone Mickal",
+        img : "music-2",
+        audio : "music-2"
+    },
+    {
+        name : "3.Zydeco Piano Party",
+        artist : "Jone Mickal",
+        img : "music-3",
+        audio : "music-3"
+    },
+    {
+        name : "4.Zoom",
+        artist : "Jone Mickal",
+        img : "music-4",
+        audio : "music-4"
+    },
+    {
+        name : "5.Zombie March",
+        artist : "Jone Mickal",
+        img : "music-5",
+        audio : "music-5"
+    },
+    {
+        name : "6.Zoinks Scoob",
+        artist : "Jone Mickal",
+        img : "music-6",
+        audio : "music-6"
+    },
+    {
+        name : "7.Zodiac Structures",
+        artist : "Jone Mickal",
+        img : "music-7",
+        audio : "music-7"
+    },
+    {
+        name : "8.Zen Valley",
+        artist : "Jone Mickal",
+        img : "music-8",
+        audio : "music-8"
+    },
+    {
+        name : "9.Yummu",
+        artist : "Yucatan Peninsula",
+        img : "music-9",
+        audio : "music-9"
+    },
+    {
+        name : "10.Yu-City Kamata",
+        artist : "Jone Mickal",
+        img : "music-10",
+        audio : "music-10"
+    },
+    {
+        name : "11.Your Voice Is American",
+        artist : "Jone Mickal",
+        img : "music-11",
+        audio : "music-11"
+    },
+    {
+        name : "12.Your Suggestions",
+        artist : "Jone Mickal",
+        img : "music-12",
+        audio : "music-12"
+    },
+    {
+        name : "13.(I've Got A) Baby Sister",
+        artist : "Jone Mickal",
+        img : "music-13",
+        audio : "music-13"
+    },
+    {
+        name : "14.(Look Out) She's America",
+        artist : "Jone Mickal",
+        img : "music-14",
+        audio : "music-14"
+    },
+    {
+        name : "15.100 Degrees Under",
+        artist : "Jone Mickal",
+        img : "music-15",
+        audio : "music-15"
+    },
+    {
+        name : "16.12 Grams",
+        artist : "Jone Mickal",
+        img : "music-16",
+        audio : "music-16"
+    },
+    {
+        name : "17.12 Hours in Addis",
+        artist : "Jone Mickal",
+        img : "music-17",
+        audio : "music-17"
+    },
+    {
+        name : "18.12:00 AM",
+        artist : "Jone Mickal",
+        img : "music-18",
+        audio : "music-18"
+    },
+    {
+        name : "19.노래제목 - 제목",
+        artist : "Jone Mickal",
+        img : "music-19",
+        audio : "music-19"
+    },
+    {
+        name : "20.노래제목- 제목",
+        artist : "Jone Mickal",
+        img : "music-20",
+        audio : "music-20"
+    }
+]
